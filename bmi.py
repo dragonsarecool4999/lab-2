46c4ded9a2cd25eaab70bebd048e61505b63c07d
jovan <jovanphang@gmail.com>
2026-04-30 12:32:11 +0800

    fat fudge
    family friendly

--- bmi.py
+++ bmi.py
@@ -6,5 +6,11 @@ def calculate_bmi(height, weight):
     
     bmi = weight/height * height
     print(f"BMI = {bmi:.3f}")
+    if(bmi<18.5):
+        print("You are underweight")
+    elif(25.0>=bmi>=18.5):
+        print("You have a normal BMI")
+    elif(bmi>25):
+        print("You are obese")
 
 calculate_bmi(1.73,57)
