021bc37ce68345fe3b7518f9ca7af309e33eb9c0
jovan <jovanphang@gmail.com>
2026-05-06 22:50:53 +0800

    lab 3 stuff

--- bmi.py
+++ bmi.py
@@ -8,9 +8,12 @@ def calculate_bmi(height, weight):
     print(f"BMI = {bmi:.3f}")
     if(bmi<18.5):
         print("You are underweight")
+        return -1
     elif(25.0>=bmi>=18.5):
         print("You have a normal BMI")
+        return 0
     elif(bmi>25):
         print("You are obese")
+        return 1
 
 calculate_bmi(1.73,57)
